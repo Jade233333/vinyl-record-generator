7bc7817e84eb70ad117bf2e38fca0a5f388471b5
Kathy <hatesjade@gmail.com>
2024-10-06 01:11:35 +0800

    restart

---
+++ scratch.py
@@ -0,0 +1,24 @@
+# import necessary libraries
+import numpy as np  # for data manipulation
+from stl import mesh  # for stl file generation
+# from testing preview
+
+
+# read depth data from txt file
+def read_depth_sequence(file_path):
+    with open(file_path, "r") as file:
+        sequence = [int(line.strip()) for line in file]
+    return np.array(sequence)
+
+
+# modify depth sequence to distance below the top surface
+def modify_depth(original, top):
+    min_value = np.min(original)
+    modified = original - min_value + top
+    return modified
+
+
+# testing
+depth_sequence = read_depth_sequence("final_4bit.out")
+print(depth_sequence)
+print(np.max(modify_depth(depth_sequence, 1)))
